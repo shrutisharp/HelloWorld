92bf5f5177546d7e90c378051b60bcdd7c2da033
Shivashankaraiah <ushivsh@chaazml-3n6df8y.corporateroot.com>
2013-08-08 20:43:21 -0700

    Initial Commit

---
+++ HelloWorldTests/HelloWorldTests.m
@@ -0,0 +1,32 @@
+//
+//  HelloWorldTests.m
+//  HelloWorldTests
+//
+//  Created by Shivashankaraiah, Shruti on 8/8/13.
+//  Copyright (c) 2013 Shivashankaraiah, Shruti. All rights reserved.
+//
+
+#import "HelloWorldTests.h"
+
+@implementation HelloWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloWorldTests");
+}
+
+@end
